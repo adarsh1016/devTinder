8416db73f1e81307b325a2481ab75069e3a8eeaf
adarsh1016 <adarshjaiswal1016@gmail.com>
2025-07-26 00:05:06 +0530

    more way of handling route

--- src/app.js
+++ src/app.js
@@ -5,24 +5,27 @@ const app = express();
     (req,res,next)=>{
     console.log("Handling the route user 1");
     next()
- },
- (req,res,next)=>{
+ });
+app.use("/user",
+    (req,res,next)=>{
     console.log("Handling the route user 2");
-    next();
- },
+    next()
+ });
+app.use("/user",
     (req,res,next)=>{
     console.log("Handling the route user 3");
     next()
- },
- (req,res,next)=>{
+ });
+app.use("/user",
+    (req,res,next)=>{
     console.log("Handling the route user 4");
     next()
- },
- (req,res,next)=>{
+ });
+app.use("/user",
+    (req,res,next)=>{
     console.log("Handling the route user 5");
     res.send("5th response!!");
- }
-);
+ });
 
 app.listen(3000,()=>{
     console.log("Server is up and listening on the port 3000..");

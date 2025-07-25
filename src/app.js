5a25566933febba476224b430f89523f04debf17
adarsh1016 <adarshjaiswal1016@gmail.com>
2025-07-25 23:58:20 +0530

    handling multiple middleware

--- src/app.js
+++ src/app.js
@@ -1,23 +1,28 @@
 const express=require("express");
 const app = express();
 
-app.get("/user",(req, res)=>{
-    res.send({firstName:"Adarsh", lastName:"Jaiswal"});
-});
-
-app.post("/user",(req,res)=>{
-    //logic for saving the data to the database 
-    res.send("Data is saved to database");
-});
-
-app.delete("/user",(req,res)=>{
-    res.send("Data has been deleted..");
-});
-
-// This app.use will respond to all HTTP call whether it being GET, POST, DELETE... nad any routes that will matches from /test to /test/abc.....
-app.use("/test",(req, res)=>{
-    res.send("response from test route");
-});
+ app.use("/user",
+    (req,res,next)=>{
+    console.log("Handling the route user 1");
+    next()
+ },
+ (req,res,next)=>{
+    console.log("Handling the route user 2");
+    next();
+ },
+    (req,res,next)=>{
+    console.log("Handling the route user 3");
+    next()
+ },
+ (req,res,next)=>{
+    console.log("Handling the route user 4");
+    next()
+ },
+ (req,res,next)=>{
+    console.log("Handling the route user 5");
+    res.send("5th response!!");
+ }
+);
 
 app.listen(3000,()=>{
     console.log("Server is up and listening on the port 3000..");

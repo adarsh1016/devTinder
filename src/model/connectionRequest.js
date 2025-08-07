6a30eee06c7df3a7c72dab4792271bd56db53fe0
adarsh1016 <adarshjaiswal1016@gmail.com>
2025-08-07 11:58:21 +0530

    created API for the sending request from A to B

---
+++ src/model/connectionRequest.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["interested", "ignored", "accepted", "rejected"],
+        message: "{VALUE} is not a valid status",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+connectionRequestSchema.pre("save", function (next) {
+  const connectionRequest = this;
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    return next(new Error("Cannot send connection request to self"));
+  }
+  next();
+});
+
+const ConnectionRequestmodel = mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema
+);
+module.exports = ConnectionRequestmodel;
--- src/routes/request.js
+++ src/routes/request.js
@@ -1,20 +1,61 @@
 const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../model/connectionRequest");
+const { User } = require("../model/user");
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  try {
-    const user = req.user;
-    //logic for sending the connections request
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
 
-    console.log("sending the connecton request");
+      const validStatuses = ["interested", "ignored"];
+      if (!validStatuses.includes(status)) {
+        return res.status(400).send("Invalid status provided");
+      }
 
-    res.send(`${user.firstName} has sent you a connection request`);
-  } catch (err) {
-    res
-      .status(400)
-      .send("Error while sending connection request: " + err.message);
+      const userExist = await User.findById(toUserId);
+      if (!userExist) {
+        return res.status(404).send("User not found");
+      }
+
+      const existingConnection = await ConnectionRequest.findOne({
+        $or: [
+          {
+            fromUserId: fromUserId,
+            toUserId: toUserId,
+            status: "interested",
+          },
+          {
+            fromUserId: toUserId,
+            toUserId: fromUserId,
+            status: "interested",
+          },
+        ],
+      });
+
+      if (existingConnection) {
+        return res.status(400).send("Connection request already exists");
+      }
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      await connectionRequest.save();
+      res.send(` ${req.user.firstName} ${status} ${userExist.firstName}`);
+    } catch (err) {
+      res
+        .status(400)
+        .send("Error while sending connection request: " + err.message);
+    }
   }
-});
+);
 
 module.exports = requestRouter;
